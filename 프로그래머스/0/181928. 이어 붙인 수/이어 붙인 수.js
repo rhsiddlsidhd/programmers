7c7842621a641f598fa9c6ffac7a625131991247
YoungJae <142131436+rhsiddlsidhd@users.noreply.github.com>
2024-10-09 15:50:55 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,20 @@
+function solution(num_list) {
+    var answer = 0;
+        // 짝수 홀수 구분하여 새로운 두 배열 구성
+    
+   const odd = num_list.filter((v,_)=>{
+        return v % 2 !==0
+    })
+   const even = num_list.filter((v,_)=>{
+       return v % 2 ===0
+   })
+    answer = Number(calculate(odd)) + Number(calculate(even))
+    return answer;
+}
+
+function calculate(inc){
+    const value = inc.reduce((acc,cur)=>{
+           return  acc += String(cur)    
+    },"")
+    return value
+}
