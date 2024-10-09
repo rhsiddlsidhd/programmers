ed4378fa3afccd5662863b2e7cb9835cf450d83b
YoungJae <142131436+rhsiddlsidhd@users.noreply.github.com>
2024-10-09 17:03:05 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,14 +1,13 @@
 function solution(num_list) {
-    var answer = [];
-    //num_list.slice(-2) // 원본훼손 X
+     //num_list.slice(-2) // 원본훼손 X
     const lastTwoElement = num_list.slice(-2);
     const value = compare(lastTwoElement)
-    answer = [...num_list, value]
-    return answer;
+    return [...num_list, value]
+    
 }
 
-function compare(inc) {
-        const [a,b] = inc;
+function compare(ary) {
+        const [a,b] = ary;
         const count = a >= b ? b * 2 : b - a
         return count
 }
