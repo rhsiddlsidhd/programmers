6e0ecb165de2e9cbc974f81244c7a1b9c5a606cd
YoungJae <142131436+rhsiddlsidhd@users.noreply.github.com>
2024-10-14 08:32:14 +0900

    [level 0] Title: 각도기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,11 +1,18 @@
 function solution(angle) {
-    if(angle > 0 && angle < 90 ) {
-        return 1 
-    } else if ( angle === 90 ){
-        return 2
-    } if(angle > 90 && angle <180){
-        return 3
-    } else {
-        return 4
-    }
+    var answer = 0;
+    
+    if(0< angle && angle < 90){
+        answer = 1
+    } else
+    if(angle === 90 ){
+        answer = 2
+    } else
+    if(90 < angle && angle < 180){
+        answer = 3
+    } else
+    {
+         answer = 4
+     }
+    
+    return answer;
 }
