2e3d4fecd3deed7391d30a007a36bc8c95ddd0da
YoungJae <142131436+rhsiddlsidhd@users.noreply.github.com>
2024-10-16 07:57:09 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.js
+++ 프로그래머스/0/120898. 편지/편지.js
@@ -1,7 +1,5 @@
 function solution(message) {
     var answer = 0;
-    [...message].forEach((_, i, arr) => {
-     answer = arr.length * 2;  
-    });
+    answer = message.length*2 
     return answer;
   }
