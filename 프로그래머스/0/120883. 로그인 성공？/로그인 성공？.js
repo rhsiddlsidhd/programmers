739ef9b2de24e22813757648dd4c995e2589db25
YoungJae <142131436+rhsiddlsidhd@users.noreply.github.com>
2024-10-25 13:30:26 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,16 +1,24 @@
   function solution(id_pw, db) {
     var answer = "fail";
    //구조분해할당
-    const inputUserId = id_pw[0];
-    const inputUserPw = id_pw[1];
-  for(let [dbId,dbPw] of db){
-      if(inputUserId === dbId){
-          if(inputUserPw === dbPw){
-             return answer = "login"
-          } else{
-            return  answer = "wrong pw"
-          }
-      }
-  }
+  //   const inputUserId = id_pw[0];
+  //   const inputUserPw = id_pw[1];
+  // for(let [dbId,dbPw] of db){
+  //     if(inputUserId === dbId){
+  //         if(inputUserPw === dbPw){
+  //            return answer = "login"
+  //         } else{
+  //           return  answer = "wrong pw"
+  //         }
+  //     }
+  // }
+      
+      //이후 map으로 리팩토링
+    const [id,pw] = id_pw;
+    const mapA = new Map(db)
+    
+ 
+answer = mapA.has(id) ? mapA.get(id) === pw ? "login" : "wrong pw" : "fail"     
+      
     return answer;
   }
