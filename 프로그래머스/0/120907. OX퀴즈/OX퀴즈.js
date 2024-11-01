6f1cb38906bf589d0884dbc69ec1303d76a163cc
YoungJae <142131436+rhsiddlsidhd@users.noreply.github.com>
2024-11-01 13:52:04 +0900

    [level 0] Title: OX퀴즈, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -1,24 +1,22 @@
 function solution(quiz) {
     var answer = [];
-    let numbers = [];
-    let operator ="";
-    let result = 0;
-
     quiz.forEach((v,_,arr)=>{
-const convert = v.split(" ").slice(0,v.split(" ").indexOf("="));
-const lastEl = v.split(" ").pop();
-       
-        for(let i = 0; i < convert.length ; i++){
-            if(isNaN(convert[i])){
-             operator = convert[i]
+const expression = v.split(" ").slice(0,v.split(" ").indexOf("="));
+const expressionResult = parseInt(v.split(" ").pop()); 
+let numbers = [];
+let operator ="";
+        for(let i = 0; i < expression.length ; i++){
+            if(isNaN(expression[i])){
+             operator = expression[i]
             } else{
-                numbers.push(parseInt(convert[i]))
+                numbers.push(parseInt(expression[i]))
             }
         }
           //연산이후 numbers 청소
+            let result;
             switch(operator){
                 case "+":    
-               result = numbers.reduce((acc,cur)=>{
+                  result = numbers.reduce((acc,cur)=>{
                   return  acc + cur
                 },0)  
                 numbers = [];
@@ -31,12 +29,7 @@ const lastEl = v.split(" ").pop();
                 break; 
                 default:
             }
-        if(result !== parseInt(lastEl)){
-            answer.push("X")
-        } else{
-            answer.push("O")
-        };
-        
+        answer.push(result === expressionResult ? "O":"X")
     })
     
     return answer;
