21eb745b7ae0c4cf4a9381c3f502c445232c9a63
YoungJae <142131436+rhsiddlsidhd@users.noreply.github.com>
2024-11-26 14:54:59 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -0,0 +1,11 @@
+function solution(x1, x2, x3, x4) { 
+    if((x1 || x2) && (x3 || x4)){
+        return true;
+    }
+    return false
+}
+
+
+//AND 둘중 모두 true 여야 true 하나라도 false 라면 false => 아래로 향한 표시
+
+//OR 둘중 하나만 true 여도 true => 위로 향한 표시
