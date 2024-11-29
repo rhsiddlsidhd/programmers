ac02f78be630f8e35e0618ca2d9e86585bb0be7e
YoungJae <142131436+rhsiddlsidhd@users.noreply.github.com>
2024-11-29 14:13:51 +0900

    [level 0] Title: 배열 만들기 4, Time: 6.13 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -2,17 +2,18 @@ function solution(arr) {
     var stk = [];
 
     let index = 0;
-    
-    while(index < arr.length){
-        if(stk.length === 0 ){
-            stk.push(arr[index])
-            index ++
-        } else if(stk[stk.length-1] < arr[index]) {
-             stk.push(arr[index])
-            index ++
-        } else if(stk[stk.length-1] >= arr[index]){
-            stk.pop();
-        } 
-    }
+
+    while (index < arr.length) {
+        if (stk.length === 0) {
+            stk.push(arr[index++]);
+        } else {
+            if (stk[stk.length - 1] < arr[index]) {
+                stk.push(arr[index++]);
+            } else {
+                stk.pop();
+            }
+        }
+    } 
+
     return stk;
 }
