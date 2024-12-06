efec912354008354edf35f0c6ce68a9a27dea485
YoungJae <142131436+rhsiddlsidhd@users.noreply.github.com>
2024-12-06 18:13:04 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 44.35 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,12 @@
+function solution(arr){
+    
+    var stack = [];
+
+    for(let i=0; i< arr.length ; i++){
+        if(stack[stack.length-1] !== arr[i]){
+            stack.push(arr[i])
+        }
+    }
+    
+    return stack;
+}
