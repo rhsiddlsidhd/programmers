05bc68a41b4a4231ce1a602e3b83b43980bd3ee8
YoungJae <142131436+rhsiddlsidhd@users.noreply.github.com>
2024-12-11 10:03:51 +0900

    [Bronze I] Title: 최소공배수, Time: 196 ms, Memory: 12268 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.js
+++ 백준/Bronze/1934. 최소공배수/최소공배수.js
@@ -1,23 +1,19 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "dev/stdin" : "example.txt";
 const input = fs.readFileSync(file).toString().trim().split("\n");
-
 input.shift();
-
 function lcm(a, b) {
   return (a * b) / gcd(a, b);
 }
 
 function gcd(a, b) {
-  while (b != 0) {
-
+  while (b !== 0) {
     [a, b] = [b, a % b];
   }
   return a;
 }
-
-for (i = 0; i < input.length; i++) {
+for (let i = 0; i < input.length; i++) {
   const [a, b] = input[i].split(" ");
+
   console.log(lcm(a, b));
 }
-
